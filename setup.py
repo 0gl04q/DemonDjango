9466528cb2d69071314cccee92d2c02bf61527bb
0gl04q <rdc_atm@mail.ru>
2024-04-26 18:58:12 +0300

    add setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DemonDjango',
+    version='1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'asgiref==3.8.1',
+        'Django==5.0.4',
+        'djangorestframework==3.15.1',
+        'sqlparse==0.5.0',
+        'tzdata==2024.1',
+    ],
+)
