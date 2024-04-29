bfd0a1bd6e8db100e4c24b254a2c5ac126e20280
0gl04q <rdc_atm@mail.ru>
2024-04-29 18:08:24 +0300

    add order

--- daemon_django_apps/info/urls.py
+++ daemon_django_apps/info/urls.py
@@ -4,5 +4,6 @@ from daemon_django_apps.info.views import get_list_info_cpu, get_table
 
 urlpatterns = [
     path('', get_list_info_cpu, name='list_info_cpu'),
-    path('table/', get_table, name='table_info_cpu')
+    path('table/', get_table, name='table_info_cpu'),
+    path('table/<str:filter_table>/<str:direction>', get_table, name='table_info_cpu_sort')
 ]
--- daemon_django_apps/info/views.py
+++ daemon_django_apps/info/views.py
@@ -4,6 +4,9 @@ from daemon_django_apps.info.models import CPUInfo
 
 
 def get_list_info_cpu(request):
+    request.session['filter_table'] = None
+    request.session['direction'] = None
+
     cpu_list_100 = CPUInfo.objects.all()[:100]
 
     min_max_avg_100 = cpu_list_100.aggregate(Min('cpu'), Max('cpu'), Avg('cpu'))
@@ -16,22 +19,17 @@ def get_list_info_cpu(request):
     })
 
 
-def get_table(request):
-    filter_table = request.GET.get('filter')
-    direction = request.GET.get('direction')
+def get_table(request, filter_table="Undefined", direction="Undefined"):
+    filter_session = request.session['filter_table']
+    direction_session = request.session['filter_table']
 
-    if filter_table:
+    if filter_table != "Undefined":
         request.session['filter_table'] = filter_table
+        cpu_list_100 = CPUInfo.objects.order_by(f'{"" if direction == "ascend" else "-"}{filter_table}')[:100]
+    elif filter_session:
+        cpu_list_100 = CPUInfo.objects.order_by(f'{"" if direction_session == "ascend" else "-"}{filter_session}')[:100]
     else:
-        filter_table = request.session['filter_table']
-
-    if direction:
-        request.session['direction'] = direction
-    else:
-        direction = request.session['direction']
-
-
-    cpu_list_100 = CPUInfo.objects.all()[:100]
+        cpu_list_100 = CPUInfo.objects.all()[:100]
 
     min_max_avg_100 = cpu_list_100.aggregate(Min('cpu'), Max('cpu'), Avg('cpu'))
     min_max_avg_all = CPUInfo.objects.aggregate(Min('cpu'), Max('cpu'), Avg('cpu'))
